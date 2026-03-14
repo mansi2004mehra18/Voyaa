0b276b67432644c6a206b629d8dbaeced21fa1d3
Mansi Mehra <mansi2004mehra18@gmail.com>
2026-03-14 23:52:49 +0530

    first commit

---
+++ classroom/server.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const app = express();
+const users = require("./routes/user.js");
+const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const sessionOptions = {
+  secret: "mysupersecretstring",
+  resave: false,
+  saveUninitialized: true,
+};
+
+const mongoose = require("mongoose");
+
+// Replace with your actual Atlas connection string
+const dbUrl = "mongodb+srv://<username>:<password>@cluster.mongodb.net/yourdbname";
+
+async function main() {
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("✅ Connected to MongoDB");
+    } catch (err) {
+        console.error("❌ MongoDB connection error:", err);
+    }
+}
+
+main();
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.successMsg = req.flash("success");
+  res.locals.errorMsg = req.flash("error");
+  next();
+});
+
+app.get("/register", (req, res) => {
+  let { name = "anonymous" } = req.query;
+  req.session.name = name;
+
+  if (name == "anonymous") {
+    req.flash("error", "user not registered");
+  } else {
+    req.flash("success", "user registered successfully!");
+  }
+  res.redirect("/hello");
+});
+
+app.get("/hello", (req, res) => {
+  res.render("page.ejs", { name: req.session.name });
+});
+
+// app.get("/reqcount", (req, res) => {
+//   if (req.session.count) {
+//     req.session.count++;
+//   } else {
+//     req.session.count = 1;
+//   }
+//   res.send(`You sent a request ${req.session.count} times`);
+// });
+
+app.get("/test", (req, res) => {
+  res.send("test successful!");
+});
+
+// app.use(cookieParser("secretcode"));
+
+// app.get("/getsignedcookie", (req, res) => {
+//   res.cookie("made-in", "India", { signed: true });
+//   res.send("signed cookie sent");
+// });
+
+// app.get("/verify", (req, res) => {
+//   console.log(req.signedCookies);
+//   res.send("verified");
+// });
+
+// app.get("/getcookies", (req, res) => {
+//   res.cookie("greet", "namaste");
+//   res.cookie("madeIn", "India");
+//   res.send("sent you some cookies!");
+// });
+
+// app.get("/greet", (req, res) => {
+//   let { name = "anonymous" } = req.cookies;
+//   res.send(`Hi, ${name}`);
+// });
+
+// app.get("/", (req, res) => {
+//   console.dir(req.cookies);
+//   res.send("Hi, I am root!");
+// });
+
+// app.use("/users", users);
+// app.use("/posts", posts);
+
+app.listen(3000, () => {
+  console.log("server is listing to 3000");
+});
